23b55054ced9b25918c1d2ed97d000409d28b552
patrick.ward <patrick.ward@octanner.com>
2014-09-10 17:05:02 -0600

    Additional functionality for Sinatra Blackjack game.

--- main.rb
+++ main.rb
@@ -21,21 +21,20 @@ helpers do
         total += a.to_i == 0 ? 10 : a.to_i
       end
     end
-    #correct for aces
+    #Account for aces
     arr.select{|element| element == "A"}.count.times do
       break if total <= BLACKJACK_AMOUNT
       total -=10
     end
     total
   end
-#This
 
   def card_image(card)
     suit = case card[0]
-             when 'H' then 'hearts'
-             when 'D' then 'diamonds'
              when 'C' then 'clubs'
              when 'S' then 'spades'
+             when 'D' then 'diamonds'
+             when 'H' then 'hearts'
            end
 
     value = card[1]
@@ -50,11 +49,29 @@ helpers do
     "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
   end
 
+  def winner!(msg)
+    @play_again = true
+    @show_hit_or_stay_buttons = false
+    @success = "<strong>#{session[:player_name]} wins! </strong> #{msg}"
+  end
+
+  def loser!(msg)
+    @play_again = true
+    @show_hit_or_stay_buttons = false
+    @error = "<strong>#{session[:player_name]} loses! </strong> #{msg}"
+  end
+
+  def tie!(msg)
+    @play_again = true
+    @show_hit_or_stay_buttons = false
+    @success = "<strong>It's a tie.</strong> #{msg}"
+  end
 end
 
 before do
   @show_hit_or_stay_buttons = true
 end
+
 # This handles when I go to localhost:9393 # if user exists, then progress to rest of the game. # Else redirect to new user forum.
 get '/' do
   if session[:player_name]
@@ -75,13 +92,15 @@ post '/new_player' do
     @error = "No ghost players allowed. Please enter a name."
     halt erb(:new_player)
   end
-  
+
   session[:player_name] = params[:player_name]
   # progress to the game, now that I have player name.
   redirect '/game'
 end
 
 get '/game' do
+  session[:turn] = session[:player_name]
+  #Create the deck and make ready for a session
   suits = ['C', 'H', 'S', 'D']
   values = ['K', 'A', 'J', 'Q', '10', '6', '2', '7', '3', '9', '4', '8', '5']
   session[:deck] = suits.product(values).shuffle!
@@ -101,19 +120,68 @@ post '/game/player/hit' do
   session[:player_cards] << session[:deck].pop
   player_total = calculate_total(session[:player_cards])
 
+  dealer_total = calculate_total(session[:dealer_cards])
   if player_total = BLACKJACK_AMOUNT
-    @success = "Way to go! #{session[:player_name]} hit blackjack"
-    @show_hit_or_stay_buttons = false
+    winner!("#{session[:player_name]} stayed at #{player_total} and dealer stayed at #{dealer_total}")
   elsif player_total > BLACKJACK_AMOUNT
-    @error = "Well, Crap!  It looks like #{session[:player_name]} busted :("
-    @show_hit_or_stay_buttons = false
+    loser!("#{session[:player_name]} overshot it at #{player_total} and dealer stayed at #{dealer_total}")
   end
   erb :game
 end
 
 post '/game/player/stay' do
-  @success = "#{session[:player_name]} has decided to stay."
+  # winner!("#{session[:player_name]} stayed at #{player_total} and dealer stayed at #{dealer_total}")
+  winner!("good job")
+  @show_hit_or_stay_buttons = false
+  redirect '/game/dealer'
+  # erb :game
+end
+
+get '/game/dealer' do
+  session[:turn] = "dealer"
   @show_hit_or_stay_buttons = false
 
+  #Determine the winner
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if dealer_total == BLACKJACK_AMOUNT
+    loser!("You lost.  Dealer hit blackjack")
+  elsif dealer_total > BLACKJACK_AMOUNT
+    winner!("Dealer busted at #{dealer_total}")
+  elsif dealer_total >= DEALER_MIN
+    #Dealer stays if between 17 and 20
+    # dealer stays: If dealer is at this point, that means player has already stayed
+    # Must mean it's time to compare hands. Redirect to compare hands
+    redirect '/game/compare'
+  else
+    # dealer hits.  Need to show the dealer hit button.  But when dealer doesn't hit, button should not show
+    @show_dealer_hit_button = true
+  end
   erb :game
+end
+
+post '/game/dealer/hit' do
+  session[:dealer_cards] << session[:deck].pop
+  redirect '/game/dealer'
+end
+
+get '/game/compare' do
+  @show_hit_or_stay_buttons = false
+  player_total = calculate_total(session[:player_cards])
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if player_total < dealer_total
+    loser!("#{session[:player_name]} stayed at #{player_total} and dealer stayed at #{dealer_total}")
+  elsif player_total > dealer_total
+    winner!("#{session[:player_name]} stayed at #{player_total} and dealer stayed at #{dealer_total}")
+
+  else
+    tie!("Both #{session[:player_name]} and dealer stayed at #{dealer_total}")
+
+  end
+  erb :game
+end
+
+get '/game_over' do
+  erb :game_over
 end
